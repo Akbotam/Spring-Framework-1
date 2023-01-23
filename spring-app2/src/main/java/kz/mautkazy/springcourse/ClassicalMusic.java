552e163c28433100d2e041b4647c1ec1808f357a
Akbotam <bmautkazy@gmail.com>
2023-01-23 17:41:45 +0600

    third commit

--- spring-app2/src/main/java/kz/mautkazy/springcourse/ClassicalMusic.java
+++ spring-app2/src/main/java/kz/mautkazy/springcourse/ClassicalMusic.java
@@ -1,8 +1,19 @@
 package kz.mautkazy.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 @Component
+//@Scope("prototype")
 public class ClassicalMusic implements Music{
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
     private String[] classicalMusic= {"Lunnaya Sonata", "Hungarian Rhapsody", "Sappy"};
     @Override
     public String[] getSong() {
