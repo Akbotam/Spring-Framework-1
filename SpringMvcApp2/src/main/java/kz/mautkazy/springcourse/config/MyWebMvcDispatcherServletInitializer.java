746e643a8bcef59f612b2da913d94a7a9f34eac5
Akbotam <bmautkazy@gmail.com>
2023-02-14 16:27:21 +0600

    Spring Mvc 2

---
+++ SpringMvcApp2/src/main/java/kz/mautkazy/springcourse/config/MyWebMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package kz.mautkazy.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
